a320fa200e668a506b6626218c03723aa5332f76
aheitzen <alexaheitzenrader@yahoo.com>
2016-07-18 20:52:02 -0700

    vanilla js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage();
-});
+// $(document).ready(function() {
+//     $('#fullpage').fullpage({
+//     	 anchors:['firstPage', 'secondPage',]
+//     });
+
+//    	$('#button1').click(function(){
+//     	$.fn.fullpage.moveSectionDown();
+// 	});
+// });
+
+$(document).ready(function (){
+            $("#button1").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#calc-portion").offset().top
+                }, 1000);
+            });
+        });
